21be5fcf415af9d55a0e4e55d405f5ed30463c88
CallanMark <Markcallan101@gmail.com>
2024-04-26 02:13:25 +0100

    Connected fork-join logic to client

--- CS4076P2Server/src/main/java/seanie/mark/cs4076p2server/controllers/ClientHandler.java
+++ CS4076P2Server/src/main/java/seanie/mark/cs4076p2server/controllers/ClientHandler.java
@@ -9,10 +9,13 @@ import java.util.Arrays;
 import java.util.List;
 import seanie.mark.cs4076p2server.exceptions.IncorrectActionException;
 import seanie.mark.cs4076p2server.models.Module;
+import java.util.concurrent.ForkJoinPool;
+
 
 public class ClientHandler implements Runnable{
     private final Socket client;
     private final List<Module> currentModules;
+    private static final ForkJoinPool forkJoinPool = new ForkJoinPool();
 
     public ClientHandler(Socket client, List<Module> currentModules){
         this.client = client;
@@ -27,15 +30,19 @@ public class ClientHandler implements Runnable{
 
             boolean running = true;
 
+
+
             while(running) {
 
                 String message = in.readLine();
                 System.out.println("Message received: " + message + " from: " + Thread.currentThread().getName());
 
                 try {
+
                     String action = message.substring(0, 2);
                     String details = message.substring(2);
-                    String newDetails = " "; 
+                    String newDetails = " ";
+
 
                     // To ensure accurate timetable info 
                     if (details.length() > 70) {
@@ -49,11 +56,23 @@ public class ClientHandler implements Runnable{
                         throw new IncorrectActionException("Incorrect action\n");
                     }
 
+                    // Local Variable declaration for EarlyLec for case "el"
+                    addModuleController addModuleController = new addModuleController(details,currentModules) ;
+                    removeModuleController removeModuleController = new removeModuleController(details,currentModules) ;
+                    CheckAvailabilityController checkAvailabilityController = new CheckAvailabilityController(details,currentModules) ;
+
                     switch (action) {
                         case "ac" -> out.println(ActionHandler.addClass(details, currentModules));
                         case "rc" -> out.println(ActionHandler.removeClass(details, currentModules));
                         case "ds" -> out.println(ActionHandler.displaySchedule(currentModules));
-                        case "el" -> out.println(new earlyLecture(details,newDetails,currentModules, 0));    
+                       // case "el" -> out.println(new EarlyLec(addModuleController , removeModuleController, checkAvailabilityController, currentModules,details)); // Removed module ok
+                        case "el" -> {
+                            EarlyLec earlyLecTask = new EarlyLec(addModuleController, removeModuleController, checkAvailabilityController, currentModules, details);
+                            String result = forkJoinPool.invoke(earlyLecTask); // Executes the task and waits for the result
+                            System.out.println(result); // Now printing the actual result of the task
+                            out.println(result);
+                        }
+                        //  case "el" -> out.println(earlyLec.compute());
                         case "st" -> {
                             System.out.println("TERMINATE");
                             out.println("TERMINATE");
@@ -63,6 +82,11 @@ public class ClientHandler implements Runnable{
                             running = false;
                         }
                     }
+
+
+
+
+
                 } catch (IncorrectActionException e) {
                     out.println(e.getMessage());
                 }
